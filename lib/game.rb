d9efc3a995733cc8395df22ab418e3961c9fb81e
Rima Jean <rima.jean@gmail.com>
2018-04-02 22:21:31 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -51,8 +51,14 @@ class Game
   end
 
   def winner
-    if win_combo = won?
-      @board.cells[win_combo[0]]
+    WIN_COMBINATIONS.detect do |win_combo|
+      if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+        return "X"
+      elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+        return "O"
+      else
+        nil
+      end
     end
   end
 
